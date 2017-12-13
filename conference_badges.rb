a294a8c2c4a7950cb5ab70f5ad260dc845caea16
Katy Carr <khcarr9@gmail.com>
2017-12-13 22:02:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}"
+end 
+
+def batch_badge_creator(array)
+  badge_array = []
+  array.each do |name|
+    string = badge_maker(name)
+    badge_array.push(string)
+  end
+  badge_array
+end
+    
